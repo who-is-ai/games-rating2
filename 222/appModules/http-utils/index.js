f66e48f3798b781bf77133b52158d87c239c6144
who-is-ai <dmitrimurasov@gmail.com>
2024-04-27 16:19:33 +0500

    final

---
+++ 222/appModules/http-utils/index.js
@@ -0,0 +1,10 @@
+const mimeTypes = require ("./mime-types");
+const parseBody = require ("./parse-body");
+const staticFile = require ("./static-file");
+
+
+module.exports = {
+    mimeTypes,
+    parseBody,
+    staticFile,
+}
